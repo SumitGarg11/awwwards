57b2c0352eddd4031d85d7db2ef3fa17fb1cf24c
SumitGarg <garg10sumit@gmail.com>
2025-03-25 18:06:18 +0530

    feat: add about section with full-screen image reveal on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./index.css";
 import Hero from "./components/Hero";
+import About from "./components/About";
 const App = () => {
   return (
     <main>
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500"></section>
+      <About/>
     </main>
   );
 };
