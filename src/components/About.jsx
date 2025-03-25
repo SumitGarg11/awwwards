da5c1091bd097b5673a2b767888ac89f528d7a5c
SumitGarg <garg10sumit@gmail.com>
2025-03-25 19:09:20 +0530

    feat: add animated title with style updates in About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React from "react";
 import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle";
 gsap.registerPlugin(ScrollTrigger);
 const About = () => {
     useGSAP(() => {
@@ -32,9 +33,8 @@ const About = () => {
           {" "}
           Welcome to Zentry{" "}
         </h2>
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem] ">
-          Disc<b>o</b>ver the world's <br />l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle title =" Disc<b>o</b>ver the world's <br />l<b>a</b>rgest shared adventure"  containerClass="mt-5 !text-black text-center" />
+       
         <div className="about-subtext">
             <p>The Game of Games begins-your life, now an epic MMORPG</p>
             <p>Zentry unites every player from countless games and platforms </p>
