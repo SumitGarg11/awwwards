41995e1f2588128f5776ce00880f3ffccff5041c
SumitGarg <garg10sumit@gmail.com>
2025-03-25 20:11:27 +0530

    feat: start implementing new features

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./index.css";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Hero />
       <About/>
       <Navbar/>
+      <Features/>
     </main>
   );
 };
